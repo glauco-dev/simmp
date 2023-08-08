379693d7e68860409a10cd7cc347837d6e4295c0
glauco-dev <icaroglauco@email.com>
2023-08-08 13:58:51 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   app/components/Carteirinha.tsx
    	modified:   package-lock.json
    	modified:   package.json

--- app/components/Carteirinha.tsx
+++ app/components/Carteirinha.tsx
@@ -4,56 +4,84 @@ import React, { useEffect, useRef, useState } from 'react'
 import html2canvas from 'html2canvas';
 
 const blacklist = ["email", "senha"]
-export default function CarteirinhaC({dados}:{dados:Afiliado}) {
-    const [_dados, setDados] = useState(dados);
-    useEffect(() => {
-        const toDataURL = (url:string) => fetch(url)
-        .then(response => response.blob())
-        .then(blob => new Promise((resolve, reject) => {
-            const reader = new FileReader()
-            reader.onloadend = () => resolve(reader.result)
-            reader.onerror = reject
-            reader.readAsDataURL(blob)
-        }))
-        toDataURL(_dados.frente.foto)
-        .then((da) => {
-            console.log("ssssssss",da);
-            setDados( {..._dados, frente:{..._dados.frente, foto: da as string}})
-        })
-    }, []);
+function fetchImageAndDownload (url:string) {
+         // Anchor href 
+
+    const img = document.createElement("img");   // Create in-memory image
+    img.addEventListener("load", () => {
+        const a = document.createElement("a");   // Create in-memory anchor
+        a.href = img.src;                        // href toward your server-image
+        a.click();                               // Trigger click (download)
+    });
+    img.src = 'fetchImage?url='+ url;       // Request image from your server
+    return img
+}
+
+
+function loadXHR(url:string) {
+
+    return new Promise(function(resolve, reject) {
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", url);
+            xhr.responseType = "blob";
+            xhr.onerror = function() {reject("Network error.")};
+            xhr.onload = function() {
+                if (xhr.status === 200) {resolve(xhr.response)}
+                else {reject("Loading error:" + xhr.statusText)}
+            };
+            xhr.send();
+        }
+        catch(err:any) {reject(err.message)}
+    });
+}
+
+export default function ({dados}:{dados:Afiliado}) {
+    const [foto, setFoto] = useState(dados.frente.foto);
+    
+
+    useEffect(()=>{
+        console.log(foto);
+        fetch(foto)
+        .then(function(response) {
+            return response.blob()
+        }).then(function(blob:any) {
+            setFoto(URL.createObjectURL(blob));
+        });
+
+    },[])
+
     const imprimirCarteira = () => {
-        console.log('!');
         let d = document.querySelector("#carteirinha-el");
         if(d){
-            const canvas = document.createElement("canvas");
-            const ctx = canvas.getContext("2d");
-            html2canvas(d as HTMLElement).then(canvas => {
+            html2canvas(d as HTMLElement, {allowTaint:true}).then(canvas => {
                 var link = document.createElement('a');
                 link.download = 'carteirinha.png';
-                link.href = canvas.toDataURL("image/");
-                canvas.getContext
+                console.log(canvas.toDataURL);
+                link.href = canvas.toDataURL("image/jpeg");
                 link.click();
             })
         } 
     }
-    return(_dados && <div id="carteirinha-el" onClick={imprimirCarteira}>
+
+    return(dados && <div id="carteirinha-el" onClick={imprimirCarteira}>
         <div className={"carteirinha frente  bg-accent-green text-white rounded-xl"}>
             <img src="logo completa branca layout alt.png" className="logo"/>
-            {Object.keys(_dados.frente).map( key => {
+            {Object.keys(dados.frente).map( key => {
                 if(blacklist.includes(key)) return
-                if(key == "foto"){
-                    return <img className="foto" src={_dados.frente[key]} />
+                if(key == "foto"){// @ts-ignore
+                    return <img key={key} className={key} src={foto} />
                 }// @ts-ignore
-                return <div key={key} className={key}><small>{key}</small>{_dados.frente[key]}</div>
+                return <div key={key} className={key}><small>{key}</small>{dados.frente[key]}</div>
             })}
         </div>
         <div className={"carteirinha atras bg-accent-green text-white rounded-xl"}>
-            {Object.keys(_dados.atras).map( key => {
+            {Object.keys(dados.atras).map( key => {
                 if(blacklist.includes(key)) return
                 if(key == "assinatura_presidente")
-                    return <img src={_dados.atras[key]} />
+                    return <img src={dados.atras[key]} />
                 // @ts-ignore
-                return <div key={key} className={key}><small>{key}</small>{_dados.atras[key]}</div>
+                return <div key={key} className={key}><small>{key}</small>{dados.atras[key]}</div>
             })}
             <div className="assinatura-wrapper">
 
