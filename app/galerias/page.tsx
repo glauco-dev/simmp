0c39c9e1064321a48961419125571f8218440a89
glauco-dev <icaroglauco@email.com>
2023-08-08 06:54:25 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   app/galerias/page.tsx

--- app/galerias/page.tsx
+++ app/galerias/page.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 const Footer = dynamic(() => import("../components/Footer"), { ssr: false })
 
 
-export default function Galerias() {
+export default function() {
     const [data, setData] = useState([] as TGaleria[]);
 
     useEffect(() => {
@@ -23,7 +23,7 @@ export default function Galerias() {
             <h2>Galerias</h2>
             {data.length > 0 ?
                 <SimpleGrid style={{ maxWidth: "90vw", paddingTop: "3rem" }} className="md:w-padrao-container-note xl:w-padrao-container  mx-auto" columns={[2, null, 3]} spacing='40px'>
-                    {data.map(doc => {
+                    {data.map(doc  => {
                         return (
                             <Link href={`/Galeria?id=${doc.id}`} style={{height:300, width:300, backgroundImage: `url(${doc.data.capa})` }} className="galerias-item">
                                 <h3>
