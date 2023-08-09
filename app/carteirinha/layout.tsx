69ead6dbd5d5114ef3df4203b750e42e2a693ae2
glauco-dev <icaroglauco@email.com>
2023-08-09 10:04:17 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   app/carteirinha/layout.tsx

--- app/carteirinha/layout.tsx
+++ app/carteirinha/layout.tsx
@@ -1,3 +1,7 @@
-export default function({children}:any){
-    return <div className="mx-auto my-10 justify-center flex" id="carteirinha-impressao">{children}</div>
+import dynamic from "next/dynamic"
+
+const Footer = dynamic(() => import("../components/Footer"), { ssr: false })
+
+export default function({children}:any){ 
+    return <><div className="mx-auto my-10 justify-center flex" id="carteirinha-impressao">{children}</div><Footer></Footer></>
 }
